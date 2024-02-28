5f0c0007aacd6d17203635a183d76a7b03a17214
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-02-28 16:32:48 +0300

    v1.0.1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,21 @@
+import heroImage from "../assets/hero-image.png";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="content-area">
+        <p className="motto">A commitment to innovation and sustainability</p>
+        <p>
+          Etudes is a pioneering firm that seamlessly merges creativity and
+          functionality to redefine architectural excellence.
+        </p>
+        <button>About us</button>
+        <div className="hero-image">
+          <img src={heroImage} alt="Logo image" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
